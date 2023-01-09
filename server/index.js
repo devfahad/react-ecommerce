4313876e313968957c9b575d061c92ff2cb086ac
devfahad <mail.devfahad@gmail.com>
2023-01-09 19:19:16 +0600

    Final

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-const express = require("express");
-const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
+import express, {json} from "express";
+import {MongoClient, ServerApiVersion, ObjectId} from "mongodb";
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
+import cors from "cors";
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mqmlnp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,8 +18,8 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
     try {
-        const db = client.db("techworld");
-        const productCollection = db.collection("product");
+        const db = client.db("tech_world");
+        const productCollection = db.collection("productCollection");
 
         app.get("/products", async (req, res) => {
             const cursor = productCollection.find({});
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Main = () => {
-    return (
-        <div className='px-10'>
-            <Navbar />
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className='px-10'>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 };
 
 export default Main;
